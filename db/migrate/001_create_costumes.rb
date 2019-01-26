02fb3e8d8fad6e670671c039c44d11f114c3bb10
Elana Ryznar <eryzna@gmail.com>
2019-01-26 01:49:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,4 @@
-class CreateCostume < ActiveRecord::Migration [4.2]
+class CreateCostume < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |costume|
       costume.string :name
